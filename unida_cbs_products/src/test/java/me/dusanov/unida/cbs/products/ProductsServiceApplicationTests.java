ded4b97c4f53024fd71f3cd0f3aa4c9f652cafec
dusanov <hrandus@yahoo.com>
2020-09-02 12:02:50 +0200

    initial product-service app

---
+++ unida_cbs_products/src/test/java/me/dusanov/unida/cbs/products/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dusanov.unida.cbs.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
